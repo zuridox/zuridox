c6312cbf8ee841a4413be1ab153c40dfb7fe0c0c
savez2223 <mosavez0786@gmail.com>
2026-01-17 21:05:07 +0530

    check

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import { ThemeProvider } from './ThemeContext.tsx'; // Import your ThemeProvider
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-// Get the root element
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root element not found");
 
-if (!rootElement) {
-  throw new Error("Root element not found! Ensure your index.html has a <div id='root'></div>.");
-}
-
-// Wrap your App with ThemeProvider
 createRoot(rootElement).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 );
